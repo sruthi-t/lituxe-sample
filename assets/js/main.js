c60b0c81075aea00504ab15b30e8629049ab1dc2
*sruthi <*sruthitk22@gmail.com*>
2022-05-01 16:38:19 +0530

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -171,3 +171,9 @@
   });
 
 })()
+  
+$(document).ready(function(){
+  document.getElementById("heart").onclick = function(){
+  document.querySelector(".fa-gratipay").style.color = "#E74C3C";
+  };
+  });
